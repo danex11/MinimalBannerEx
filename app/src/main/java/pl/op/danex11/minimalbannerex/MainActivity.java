0719f6949ce6d90b44faf2c5d047014110da75e7
danex11 <danex11@op.pl>
2021-10-18 06:35:26 +0200

    comment as steps 123

--- app/src/main/java/pl/op/danex11/minimalbannerex/MainActivity.java
+++ app/src/main/java/pl/op/danex11/minimalbannerex/MainActivity.java
@@ -7,8 +7,12 @@ import android.os.Bundle;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,18 +21,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // AdMob 1 : add view
         AdView mAdView = (AdView) findViewById(R.id.adView);
+        //AdMob 2 : initialize
         //MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
             }
         });
+        //AdMob 3 : request and load an ad
         AdRequest adRequest = new AdRequest.Builder()
                 .build();
         mAdView.loadAd(adRequest);
 
-
+        //attempt to add emulator as a test device
+        List<String> testDeviceIds = Arrays.asList("33BE2250B43518CCDA7DE426D04EE231");
+        RequestConfiguration configuration =
+                new RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
+        MobileAds.setRequestConfiguration(configuration);
 
 
 
